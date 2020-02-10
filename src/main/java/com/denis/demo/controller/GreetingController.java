2f68dece2150cea8bd0eb526c8f393d4f1b0d562
denisshchurin <denisshchurin@gmail.com>
2020-02-10 12:21:57 +0300

    change class GreetingController

--- src/main/java/com/denis/demo/controller/GreetingController.java
+++ src/main/java/com/denis/demo/controller/GreetingController.java
@@ -3,15 +3,21 @@ package com.denis.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required = false,
-            defaultValue = "World") String name, Model model){
-        model.addAttribute("name", name);
+    public String greetingForm(Model model){
+        model.addAttribute("name", new Greeting());
         return "greeting";
+    }
+
+    @PostMapping("/greeting")
+    public String greeetingSubmit(@ModelAttribute Greeting greeting){
+        return "result";
     }
 }
